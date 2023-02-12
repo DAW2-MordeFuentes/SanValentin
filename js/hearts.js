8dfa76677b6187a8db9e4b74d77ad24a8be22eba
DAW2-MordeFuentes <125151110+DAW2-MordeFuentes@users.noreply.github.com>
2023-02-12 23:34:06 +0100

    Add files via upload

--- js/hearts.js
+++ js/hearts.js
@@ -98,7 +98,7 @@ function drawHeart(posicionCorazones) {
         clearInterval(intervalCorazones)
         setTimeout(() => {
             lastInterval = setInterval(final, 50)    
-        }, 3000);
+        }, 5000);
     }
 }
 
